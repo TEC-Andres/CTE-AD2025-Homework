987f7a65c652ad5ed43e5ac0a0eb66f282464017
ICE MrThink15000 <a01287002@tec.mx>
2025-08-22 13:59:07 -0600

    Added header comments

--- ClassAct 2/classwork4.py
+++ ClassAct 2/classwork4.py
@@ -1,3 +1,14 @@
+'''
+#       Sesion 2: Introductory Activity
+#       Andrés Rodríguez Cantú ─ A01287002
+#
+#       Copyright (C) Tecnológico de Monterrey
+#
+#       File: classwork4.py
+#
+#       Created:                08/22/2025
+#       Last Modified:          08/22/2025
+'''
 purchase = 1200
 if purchase < 100:
     print("Pay with cash")
--- ClassAct 2/classwork5.py
+++ ClassAct 2/classwork5.py
@@ -1,3 +1,14 @@
+'''
+#       Sesion 2: Introductory Activity
+#       Andrés Rodríguez Cantú ─ A01287002
+#
+#       Copyright (C) Tecnológico de Monterrey
+#
+#       File: classwork4.py
+#
+#       Created:                08/22/2025
+#       Last Modified:          08/22/2025
+'''
 age = int(input("How old are you? "))
 if age >= 18:
     print("You are an adult.")
