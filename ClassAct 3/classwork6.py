b9813b7e33091ba5dc450992ed286a03745115a1
ICE MrThink15000 <a01287002@tec.mx>
2025-08-29 19:02:33 -0600

    Added classworks made in Friday

---
+++ ClassAct 3/classwork6.py
@@ -0,0 +1,16 @@
+'''
+#       Sesion 3: Functions
+#       Andrés Rodríguez Cantú ─ A01287002
+#
+#       Copyright (C) Tecnológico de Monterrey
+#
+#       File: classwork6.py
+#
+#       Created:                08/29/2025
+#       Last Modified:          08/29/2025
+'''
+def getName(name):
+    return name.upper(), name.title(), '. '.join([word[0].upper() for word in name.split() if word]) + '.'    
+
+name = input("Enter your name: ")
+print('\n'.join(getName(name)))
